43ed3254268d7636bef20e7b666c297479cbc253
gmoehler <gmoeh@gmx.de>
2017-09-07 22:06:10 +0200

    update with client server sample

--- wifi/src/main.cpp
+++ wifi/src/main.cpp
@@ -22,9 +22,10 @@
 static bool wifi_connected = false;
 static bool done=false;
 
-const char* ssid     = "xxxx";
-const char* password = "yyyy";
+const char* ssid     = "My_SSID";
+const char* password = "mypassword";
 
+WiFiServer server(80);
 
 // https://github.com/espressif/arduino-esp32/blob/ca7f6cc516ce842cf7c6b35dbde4b0d4a5a86404/tools/sdk/include/esp32/esp_event.h
 
@@ -207,6 +208,60 @@ void startWps(){
 
 }
 
+void serverStartup(){
+  WiFiClient client = server.available();   // listen for incoming clients
+  
+  if (client) {                             // if you get a client,
+    Serial.println("New Client.");           // print a message out the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      Serial.println("Client connected");
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        if (c == '\n') {                    // if the byte is a newline character
+
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+
+            // the content of the HTTP response follows the header:
+            client.print("Click <a href=\"/H\">here</a> to turn the LED on pin 5 on.<br>");
+            client.print("Click <a href=\"/L\">here</a> to turn the LED on pin 5 off.<br>");
+
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          } else {    // if you got a newline, then clear currentLine:
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+
+        // Check to see if the client request was "GET /H" or "GET /L":
+        if (currentLine.endsWith("GET /H")) {
+          Serial.println("HIGH");
+          //digitalWrite(5, HIGH);               // GET /H turns the LED on
+        }
+        if (currentLine.endsWith("GET /L")) {
+          Serial.println("LOW");
+          //digitalWrite(5, LOW);                // GET /L turns the LED off
+        }
+      }
+    }
+    // close the connection:
+    client.stop();
+    Serial.println("Client Disconnected.");
+  }
+}
+
 void initWifiConnect(){
 
   WiFi.begin(ssid, password);
@@ -237,7 +292,8 @@ void loop()
 {
     delay(1000);
     if (wifi_connected && !done) {
-      testClient("http://vvs.de", 80);
+      //testClient("http://vvs.de", 80);
+      serverStartup();
       done = true;
     }
 
