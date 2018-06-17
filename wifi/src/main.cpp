99630711342e3de15da3b80f2a293e810bc1be6c
Gregor Moehler <gmoeh@gmx.de>
2018-06-18 00:44:14 +0200

    working proc for eps32-idf

--- wifi/src/main.cpp
+++ wifi/src/main.cpp
@@ -12,16 +12,21 @@
 #include "esp_system.h"
 #include "esp_wifi.h"
 #include "esp_event_loop.h"
-#include "esp_log.h"
 #include "nvs_flash.h"
+#include "IPAddress.h"
 
 #include "lwip/err.h"
 #include "lwip/sys.h"
 
-#define SSID "MY_SSID"
+#define SSID "MY_SSID7"
 #define PASSWORD "my_password"
 
 #define EXAMPLE_MAX_STA_CONN       CONFIG_MAX_STA_CONN
+
+IPAddress myIP(192,168,1,127);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
+uint16_t port = 1110;
 
 int level = 0;
 
@@ -33,34 +38,58 @@ static EventGroupHandle_t wifi_event_group;
    to the AP with an IP? */
 const int WIFI_CONNECTED_BIT = BIT0;
 
-static const char *TAG = "simple wifi";
+const char * system_event_reasons[] = { "UNSPECIFIED", "AUTH_EXPIRE", "AUTH_LEAVE", "ASSOC_EXPIRE", "ASSOC_TOOMANY", "NOT_AUTHED", "NOT_ASSOCED", "ASSOC_LEAVE", "ASSOC_NOT_AUTHED", "DISASSOC_PWRCAP_BAD", "DISASSOC_SUPCHAN_BAD", "IE_INVALID", "MIC_FAILURE", "4WAY_HANDSHAKE_TIMEOUT", "GROUP_KEY_UPDATE_TIMEOUT", "IE_IN_4WAY_DIFFERS", "GROUP_CIPHER_INVALID", "PAIRWISE_CIPHER_INVALID", "AKMP_INVALID", "UNSUPP_RSN_IE_VERSION", "INVALID_RSN_IE_CAP", "802_1X_AUTH_FAILED", "CIPHER_SUITE_REJECTED", "BEACON_TIMEOUT", "NO_AP_FOUND", "AUTH_FAIL", "ASSOC_FAIL", "HANDSHAKE_TIMEOUT" };
+#define reason2str(r) ((r>176)?system_event_reasons[r-176]:system_event_reasons[r-1])
 
 static esp_err_t event_handler(void *ctx, system_event_t *event)
 {
+    uint8_t* reason;
     switch(event->event_id) {
-    case SYSTEM_EVENT_STA_START:
+        case SYSTEM_EVENT_WIFI_READY:
+        printf("Wifi ready.\n");
+        break;
+        case SYSTEM_EVENT_SCAN_DONE:
+        printf("Wifi scan done.\n");
+        break;
+        case SYSTEM_EVENT_STA_START:
+        printf("sta started.\n");
         esp_wifi_connect();
         break;
-    case SYSTEM_EVENT_STA_GOT_IP:
-        ESP_LOGI(TAG, "got ip:%s",
+        case SYSTEM_EVENT_STA_STOP:
+        printf("Wifi disconnected.\n");
+        break;
+        case SYSTEM_EVENT_STA_CONNECTED:
+        printf("Wifi connected.\n");
+        break;
+        case SYSTEM_EVENT_STA_AUTHMODE_CHANGE:
+        printf("Authmode of Access Point has changed.\n");
+        break;
+        case SYSTEM_EVENT_STA_GOT_IP:
+        printf( "got ip:%s\n",
                  ip4addr_ntoa(&event->event_info.got_ip.ip_info.ip));
         xEventGroupSetBits(wifi_event_group, WIFI_CONNECTED_BIT);
         break;
-    case SYSTEM_EVENT_AP_STACONNECTED:
-        ESP_LOGI(TAG, "station:"MACSTR" join, AID=%d",
-                 MAC2STR(event->event_info.sta_connected.mac),
-                 event->event_info.sta_connected.aid);
-        break;
-    case SYSTEM_EVENT_AP_STADISCONNECTED:
-        ESP_LOGI(TAG, "station:"MACSTR"leave, AID=%d",
-                 MAC2STR(event->event_info.sta_disconnected.mac),
-                 event->event_info.sta_disconnected.aid);
-        break;
-    case SYSTEM_EVENT_STA_DISCONNECTED:
+        case SYSTEM_EVENT_STA_DISCONNECTED:
+        printf("WiFi disconnected ");
         esp_wifi_connect();
         xEventGroupClearBits(wifi_event_group, WIFI_CONNECTED_BIT);
+        reason = &event->event_info.disconnected.reason;
+        printf("Reason: %u - %s\n", reason, reason2str(*reason));
         break;
-    default:
+        case SYSTEM_EVENT_STA_WPS_ER_SUCCESS:
+        printf("WiFi connected by WPS.\n");
+        break;
+        case SYSTEM_EVENT_STA_WPS_ER_FAILED:
+        printf("WPS connection failed.\n");
+        break;
+        case SYSTEM_EVENT_STA_WPS_ER_TIMEOUT:
+        printf("WPS connection timeout.\n");
+        break;
+        case SYSTEM_EVENT_STA_WPS_ER_PIN:
+        printf("WPS with pin mode: %s\n"/*, PIN2STR(event.event_info.sta_er_pin.pin_code)*/);
+        break;
+        default:
+        printf("[WiFi-event] Unhandled event: %d\n", event);
         break;
     }
     return ESP_OK;
@@ -86,14 +115,37 @@ void wifi_init_sta()
     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &sta_config) );
     ESP_ERROR_CHECK(esp_wifi_start() );
 
-    ESP_LOGI(TAG, "wifi_init_sta finished.");
-    ESP_LOGI(TAG, "connect to ap SSID:%s password:%s",
+    tcpip_adapter_ip_info_t info;
+
+    if(myIP != (uint32_t)0x00000000){
+        info.ip.addr = static_cast<uint32_t>(myIP);
+        info.gw.addr = static_cast<uint32_t>(gateway);
+        info.netmask.addr = static_cast<uint32_t>(subnet);
+    } else {
+        info.ip.addr = 0;
+        info.gw.addr = 0;
+        info.netmask.addr = 0;
+    }
+
+    esp_err_t err = tcpip_adapter_dhcpc_stop(TCPIP_ADAPTER_IF_STA);
+    if(err != ESP_OK && err != ESP_ERR_TCPIP_ADAPTER_DHCP_ALREADY_STOPPED){
+        printf("DHCP could not be stopped! Error: %d", err);
+    }
+
+    err = tcpip_adapter_set_ip_info(TCPIP_ADAPTER_IF_STA, &info);
+    if(err != ERR_OK){
+        printf("STA IP could not be configured! Error: %d", err);
+    }
+
+    printf( "wifi_init_sta finished.\n");
+    printf( "connect to ap SSID:%s password:%s\n",
              SSID, PASSWORD);
 }
 
 void setup()
 {
     //Initialize NVS
+    printf( "init nvs.\n");
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES) {
       ESP_ERROR_CHECK(nvs_flash_erase());
@@ -101,15 +153,24 @@ void setup()
     }
     ESP_ERROR_CHECK(ret);
     
-    ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
+    printf( "ESP_WIFI_MODE_STA\n");
     wifi_init_sta();
+
+    vTaskDelay(6000 / portTICK_PERIOD_MS);
+    printf("disconnecting...\n");
+    esp_wifi_disconnect();
+
+    vTaskDelay(6000 / portTICK_PERIOD_MS);
+    printf("stopping...\n");
+    esp_wifi_stop();
+
 }
 
 
 void loop()
 {
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    // vTaskDelay(1000 / portTICK_PERIOD_MS);
 
-    gpio_set_level(GPIO_NUM_4, level);
-    level = !level;
+    // gpio_set_level(GPIO_NUM_4, level);
+    // level = !level;
 }
